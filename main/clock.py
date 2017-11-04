1c15979722ab8747371036767a09afd913a3e085
RioBithub <30948099+RioBithub@users.noreply.github.com>
2017-11-04 23:02:59 +0700

    Update clock.py

--- main/clock.py
+++ main/clock.py
@@ -3,6 +3,8 @@ import os, sys, time
 os.system("sh termux")
 print '==================================================='
 os.system("echo ~~Clock script")
+os.system("echo Requirements is Finded")
+os.system("python speed.py')
 print '==================================================='
 print '[Made by Alu from One Evo]'
 print '[Copyright will get banned]'
