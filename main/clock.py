0c78de70ed4179c044bc77ef128aa6affb7c059a
RioBithub <30948099+RioBithub@users.noreply.github.com>
2017-11-04 22:50:45 +0700

    Add files via upload

---
+++ main/clock.py
@@ -0,0 +1,16 @@
+from time import gmtime, strftime
+import os, sys, time
+os.system("sh termux")
+print '==================================================='
+os.system("echo ~~Clock script")
+print '==================================================='
+print '[Made by Alu from One Evo]'
+print '[Copyright will get banned]'
+data = strftime("[Year]%Y\n[Month]%m\n[Day]%d\n[Clock] %H:%M:%S", gmtime())
+total = time.time()
+os.system("echo Loading data")
+totol = time.time() -total
+bk = ('Speed cmd:%s' % (totol))
+print (data)
+print (bk)
+
